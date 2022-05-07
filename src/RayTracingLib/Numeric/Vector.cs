5d8b8bac08ee2c2a156676bf58d939284c74835a
ShkarupaDC <dimon.shkarupa@gmail.com>
2022-05-07 23:42:01 +0300

    Implement Vector3, cover it with tests

---
+++ src/RayTracingLib/Numeric/Vector.cs
@@ -0,0 +1,58 @@
+namespace RayTracingLib.Numeric
+{
+    public class Vector3
+    {
+        private const float EPS = 1E-6F;
+        public float X { get; }
+        public float Y { get; }
+        public float Z { get; }
+
+        public Vector3(float x, float y, float z)
+        {
+            X = x; Y = y; Z = z;
+        }
+
+        public Vector3(float value)
+        {
+            X = Y = Z = value;
+        }
+
+        public float Length() => (float)Math.Sqrt(LengthSquared());
+
+        public float LengthSquared() => Dot(this, this);
+
+        public static Vector3 operator +(Vector3 left, Vector3 right) =>
+            new(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+
+        public static Vector3 operator -(Vector3 left, Vector3 right) =>
+            new(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+
+        public static Vector3 operator -(Vector3 value) =>
+            new(-value.X, -value.Y, -value.Z);
+
+        public static Vector3 operator *(Vector3 value, float factor) =>
+            new(value.X * factor, value.Y * factor, value.Z * factor);
+
+        public static Vector3 operator /(Vector3 value, float factor) =>
+            new(value.X / factor, value.Y / factor, value.Z / factor);
+
+        public static bool operator ==(Vector3 left, Vector3 right) =>
+            (left - right).LengthSquared() < EPS * EPS;
+
+        public static bool operator !=(Vector3 left, Vector3 right) =>
+            !(left == right);
+
+        public static Vector3 Normalize(Vector3 value) =>
+            value / value.Length();
+
+        public static Vector3 Cross(Vector3 left, Vector3 right) =>
+            new(
+                left.Y * right.Z - left.Z * right.Y,
+                left.Z * right.X - left.X * right.Z,
+                left.X * right.Y - left.Y * right.X
+            );
+
+        public static float Dot(Vector3 left, Vector3 right) =>
+            left.X * right.X + left.Y * right.Y + left.Z * right.Z;
+    }
+}
---
+++ tests/RayTracingLib.Tests/Numeric/VectorTests.cs
@@ -0,0 +1,87 @@
+namespace RayTracingLib.Tests.Numeric
+{
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
+    using RayTracingLib.Numeric;
+
+    public class VectorTests
+    {
+        public const float SQRT_EPS = 1E-15F;
+
+        public static IEnumerable<object[]> GetSumData()
+        {
+            yield return new object[] {
+                new Vector3(-11, 3, 7), new Vector3(5, 6, -3), new Vector3(-6, 9, 4)
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSumData))]
+        public void SumTest(Vector3 left, Vector3 right, Vector3 expected)
+        {
+            Assert.True(expected == left + right);
+            Assert.True(expected == right + left);
+        }
+
+        public static IEnumerable<object[]> GetDotData()
+        {
+            yield return new object[] {
+                new Vector3(1, 0, 0), new Vector3(0, 1, 0), 0F
+            };
+            yield return new object[] {
+                new Vector3(1, 0, 0), new Vector3(-1, 0, 0), -1F
+            };
+            yield return new object[] {
+                new Vector3(1, 0, 0), new Vector3(1, 0, 0), 1F
+            };
+            yield return new object[] {
+                new Vector3(4, -2, 1), new Vector3(-7, 3, 12), -22F
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDotData))]
+        public void DotTest(Vector3 left, Vector3 right, float expected)
+        {
+            Assert.Equal(expected, Vector3.Dot(left, right));
+        }
+
+        public static IEnumerable<object[]> GetCrossData()
+        {
+            yield return new object[] {
+                new Vector3(2, 1, -6), new Vector3(0, 0, 0)
+            };
+            yield return new object[] {
+                new Vector3(3, -5, 2), new Vector3(-1, 7, 4)
+            };
+            yield return new object[] {
+                new Vector3(1, 0, 0), new Vector3(0, 1, 0)
+            };
+        }
+
+        public static float GetAngleSine(Vector3 left, Vector3 right)
+        {
+            var lenProduct = (float)Math.Sqrt(left.LengthSquared() * right.LengthSquared());
+            if (lenProduct < SQRT_EPS)
+            {
+                return 1F;
+            }
+            var angle = Vector3.Dot(left, right) / lenProduct;
+            return (float)Math.Sqrt(1 - Math.Pow(angle, 2));
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCrossData))]
+        public void CrossTest(Vector3 left, Vector3 right)
+        {
+            var cross = Vector3.Cross(left, right);
+            var area = left.Length() * right.Length() * GetAngleSine(left, right);
+
+            Assert.Equal(area, cross.Length());
+            Assert.Equal(0, Vector3.Dot(left, cross));
+            Assert.Equal(0, Vector3.Dot(right, cross));
+        }
+    }
+}
